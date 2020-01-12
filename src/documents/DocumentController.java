8859bea2fab997a040a63a6fae392d1332dbd699
Mithilesh Parmar <mithileshparmar1@gmail.com>
2020-01-12 11:29:12 +0530

    fix: search for document added

--- src/documents/DocumentController.java
+++ src/documents/DocumentController.java
@@ -2,10 +2,14 @@ package documents;
 
 import com.google.cloud.firestore.QuerySnapshot;
 import custom_view.SearchTextFieldController;
+import custom_view.card_view.Card;
 import documents.add_document.AddDocumentController;
 import documents.add_document.AddDocumentListener;
 import javafx.beans.property.BooleanProperty;
+import javafx.beans.property.ListProperty;
 import javafx.beans.property.SimpleBooleanProperty;
+import javafx.beans.property.SimpleListProperty;
+import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
@@ -23,6 +27,7 @@ import model.Student;
 import model.StudentDocument;
 import students.detail_view.StudentDetailsController;
 import utility.CloudStorageStudentDocumentListener;
+import utility.SearchCallback;
 import utility.StudentDocumentCloudStorageUtility;
 
 import java.io.File;
@@ -31,7 +36,8 @@ import java.net.URL;
 import java.util.List;
 import java.util.ResourceBundle;
 
-public class DocumentController implements Initializable, DataChangeListener, CloudStorageStudentDocumentListener {
+
+public class DocumentController implements Initializable, DataChangeListener, CloudStorageStudentDocumentListener, SearchCallback {
 
     public TableView<StudentDocument> documentTable;
     public SearchTextFieldController searchTextField;
@@ -42,9 +48,11 @@ public class DocumentController implements Initializable, DataChangeListener, Cl
     private BooleanProperty dataLoading = new SimpleBooleanProperty(true);
 
 
+
+
     @Override
     public void initialize(URL url, ResourceBundle resourceBundle) {
-//        addButton.setPadding(new Insets(14));
+        searchTextField.setCallback(this);
         addButton.setOnAction(actionEvent -> loadAddView());
         refreshButton.setOnAction(actionEvent -> refreshData());
         cloudStorageUtility.setListener(this);
@@ -52,7 +60,7 @@ public class DocumentController implements Initializable, DataChangeListener, Cl
         progressIndicator.visibleProperty().bind(dataLoading);
         cloudStorageUtility.getAllDocuments();
         refreshButton.visibleProperty().bind(dataLoading.not());
-        documentTable.itemsProperty().bind(cloudStorageUtility.studentDocuments);
+        documentTable.setItems(cloudStorageUtility.studentDocuments);
         documentTable.setOnKeyPressed(keyEvent -> handleOnTableClick(keyEvent, documentTable.getSelectionModel().getSelectedItem()));
         documentTable.setOnContextMenuRequested(event -> {
             showContextMenu(event, documentTable.getSelectionModel().getSelectedItem());
@@ -73,6 +81,7 @@ public class DocumentController implements Initializable, DataChangeListener, Cl
         contextMenu.setHideOnEscape(true);
 
         MenuItem deleteButton = new MenuItem("Delete");
+
         deleteButton.setOnAction(actionEvent -> showConfirmationAlert(selectedItem));
 
         MenuItem cancelButton = new MenuItem("Cancel");
@@ -172,10 +181,38 @@ public class DocumentController implements Initializable, DataChangeListener, Cl
     @Override
     public void onFinish() {
         dataLoading.set(false);
+        refreshData();
     }
 
     @Override
     public void onStart() {
         dataLoading.set(true);
+    }
+
+    @Override
+    public void performSearch(String oldValue, String newValue) {
+        if (dataLoading.get()) return;
+        // if pressing backspace then set initial values to list
+        if (oldValue != null && (newValue.length() < oldValue.length())) {
+            documentTable.itemsProperty().set(cloudStorageUtility.studentDocuments);
+        }
+
+        // convert the searched text to uppercase
+        String searchtext = newValue.toUpperCase();
+
+
+        ListProperty<StudentDocument> subList = new SimpleListProperty<>(FXCollections.observableArrayList());
+        for (StudentDocument p : cloudStorageUtility.studentDocuments) {
+            String text =
+                    p.getFileName().toUpperCase() + " "
+                            + p.getStudentAdmissionNumber().toUpperCase();
+            // if the search text contains the manufacturer then add it to sublist
+            if (text.contains(searchtext)) {
+                subList.get().add(p);
+            }
+
+        }
+        // set the items to tableView that matches
+        documentTable.itemsProperty().setValue(subList);
     }
 }
