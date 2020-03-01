002a40cb0ad427b607a527972af74b5897e2fce0
Mithilesh Parmar <mithileshparmar1@gmail.com>
2020-03-01 11:05:10 +0530

    update: menu toggle button updated with icons

--- src/home/HomeController.java
+++ src/home/HomeController.java
@@ -7,8 +7,10 @@ import javafx.collections.ObservableList;
 import javafx.fxml.FXMLLoader;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
+import javafx.geometry.Pos;
 import javafx.scene.Node;
 import javafx.scene.control.*;
+import javafx.scene.image.ImageView;
 import javafx.scene.layout.Background;
 import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.VBox;
@@ -46,38 +48,6 @@ public class HomeController implements Initializable {
         });
 
         toggleGroup.selectToggle(menuList.get(0));
-
-
-//        mainMenu.setItems(menuList);
-
-
-//        mainMenu.setCellFactory(new Callback<>() {
-//
-//            public ListCell<Menu> call(ListView<Menu> param) {
-//
-//                return new ListCell<>() {
-//                    @Override
-//                    public void updateItem(Menu item, boolean empty) {
-//                        super.updateItem(item, empty);
-//                        if (item != null) {
-//                            setText(item.getName());
-//                        }
-//                    }
-//                };
-//            }
-//        });
-
-
-//        mainMenu.getSelectionModel().selectedItemProperty().addListener(((observableValue, menu, t1) -> {
-//            try {
-//                changePageTo(FXMLLoader.load(getClass().getResource(observableValue.getValue().getScreenPath())));
-//            } catch (IOException e) {
-//                e.printStackTrace();
-//            }
-//        }
-//        ));
-
-//        mainMenu.getSelectionModel().selectFirst();
     }
 
 
@@ -91,32 +61,36 @@ public class HomeController implements Initializable {
     }
 
     private void setUpMenu() {
-        menuList.add(getScreenButton("Back Logs", "/backlog/BackLogView.fxml", ""));
-        menuList.add(getScreenButton("Fee", "/fee/FeeView.fxml", ""));
-        menuList.add(getScreenButton("Students", "/students/StudentView.fxml", ""));
-        menuList.add(getScreenButton("Teachers", "/teachers/TeacherView.fxml", ""));
-        menuList.add(getScreenButton("Events", "/events/EventsView.fxml", ""));
-        menuList.add(getScreenButton("Time Table", "/time_table/TimeTableView.fxml", ""));
-        menuList.add(getScreenButton("Course", "/course/CourseView.fxml", ""));
-        menuList.add(getScreenButton("Teacher Leaves", "/teacher_leaves/LeavesView.fxml", ""));
-        menuList.add(getScreenButton("Attendance", "/attendance/AttendanceView.fxml", ""));
-        menuList.add(getScreenButton("Documents", "/documents/DocumentView.fxml", ""));
-        menuList.add(getScreenButton("Exams", "/exams/ExamView.fxml", ""));
-        menuList.add(getScreenButton("Result", "/result/ResultView.fxml", ""));
+        menuList.add(getScreenButton("Back Logs", "/backlog/BackLogView.fxml", "/assets/backlog.png"));
+        menuList.add(getScreenButton("Fee", "/fee/FeeView.fxml", "/assets/fee.png"));
+        menuList.add(getScreenButton("Students", "/students/StudentView.fxml", "/assets/student.png"));
+        menuList.add(getScreenButton("Teachers", "/teachers/TeacherView.fxml", "/assets/teacher.png"));
+        menuList.add(getScreenButton("Events", "/events/EventsView.fxml", "/assets/event.png"));
+        menuList.add(getScreenButton("Time Table", "/time_table/TimeTableView.fxml", "/assets/timetable.png"));
+        menuList.add(getScreenButton("Course", "/course/CourseView.fxml", "/assets/course.png"));
+        menuList.add(getScreenButton(" Leaves", "/teacher_leaves/LeavesView.fxml", "/assets/leave.png"));
+        menuList.add(getScreenButton("Attendance", "/attendance/AttendanceView.fxml", "/assets/attendance.png"));
+        menuList.add(getScreenButton("Documents", "/documents/DocumentView.fxml", "/assets/documents.png"));
+        menuList.add(getScreenButton("Exams", "/exams/ExamView.fxml", "/assets/exam.png"));
+        menuList.add(getScreenButton("Result", "/result/ResultView.fxml", "/assets/backlog.png"));
     }
 
 
-    private ToggleButton getScreenButton(String title, String screenPath, String test) {
+    private ToggleButton getScreenButton(String title, String screenPath, String iconPath) {
 
         ToggleButton toggleButton = new ToggleButton(title);
-        toggleButton.setPrefHeight(35);
         toggleButton.setMinHeight(35);
-        toggleButton.setMaxHeight(35);
+        toggleButton.setMaxHeight(55);
 
-        toggleButton.setPrefWidth(120);
         toggleButton.setMinWidth(100);
-        toggleButton.setMaxWidth(150);
+        toggleButton.setMaxWidth(200);
+        toggleButton.setAlignment(Pos.CENTER_LEFT);
 
+        ImageView imageView = new ImageView(iconPath);
+        imageView.setFitWidth(30);
+        imageView.setFitHeight(30);
+
+        toggleButton.setGraphic(imageView);
         toggleButton.setUserData(screenPath);
         toggleButton.setId("menu-toggle");
         return toggleButton;
